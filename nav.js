aa95eef34125c9289ae31c7f04be0b23368d3bb7
TIng-Yuan Cheng <r05922098@ntu.edu.tw>
2018-01-27 15:23:11 +0000

    go3

--- nav.js
+++ nav.js
@@ -1,14 +1,30 @@
 
 $(window).on('scroll', function () {
-    var el = $('.menubar');
-    var tx = el.position().top; // + el.outerHeight(true);
-    if ($(this).scrollTop() > tx) {
-        if (!$('.menubar').hasClass('menubar-nav')) {
-            $('.menubar').addClass('menubar-nav');
+    var setItemClass = function(element, className, state){
+        if(state){
+            if (!element.hasClass(className)) {
+               element.addClass(className);
+            }
+        } else {
+            if (element.hasClass(className)) {
+                element.removeClass(className);
+            }
         }
-    } else {
-        if ($('.menubar').hasClass('menubar-nav')) {
-            $('.menubar').removeClass('menubar-nav');
-        }
-    }
+    };
+    var tx = $('.navbar').position().top; // + el.outerHeight(true);
+    var show = ($(this).scrollTop() > tx) ? 1 : 0;
+    
+    var el2 = $('.content-a-inner > h1');
+    var tx2 = el2.position().top + el2.offset().top + el2.outerHeight(true);
+    var show2 = ($(this).scrollTop() + $('.menubar').height() > tx2) ? 1 : 0;
+    setItemClass($('.navbar'), 'navbar-fixed', show);
+    setItemClass($('.menubar'), 'menubar-fixed', show);
+    setItemClass($('.menu_mark'), 'menu_mark-fixed', show);
+});
+$(window).bind('hashchange', function() {
+    //console.log($(this).scrollTop());
+    $('html, body').css('scroll-top',$(this).scrollTop() - $('.navbar-fixed').height());
+    /*$('html, body').animate({
+        scrollTop: $(this).scrollTop() - $('.navbar-fixed').height()
+    }, 1000);*/
 });
