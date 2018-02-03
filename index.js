f69e91f1cd1d6047b217815be1c2f8fb5d849cd1
DeemoHarlos <cces4415@gmail.com>
2018-02-04 01:09:17 +0800

    complete navbar

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ generateHexBlocks(howBlocks)
 generateRoadmap()
 
 $("#title #whitepaper").click(()=>{alert('To be announced')})
+
+document.addEventListener('scroll', function(){
+	if ($("html").scrollTop < 400) $("#navbar").style.opacity = 0;
+	else $("#navbar").style.opacity = 1;
+});
