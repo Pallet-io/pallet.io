78b5c90b5f1eee057bf9100401dcb52cc9ae6333
DeemoHarlos <cces4415@gmail.com>
2018-02-04 23:16:38 +0800

    Add language support, add block hover shadow

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ generateHexBlocks(howBlocks)
 
 generateRoadmap()
 
+$("#title>#lang>select").addEventListener("change", function(){
+	var lang = $("#title>#lang>select").value
+	if(lang == "en") window.location = "index.html"
+	else window.location = "index_" + lang + ".html"
+});
+
 function toBeAnnounced(a){
 	if(a == "en") alert("To be announced!")
 	if(a == "zh-tw") alert("即將發佈！")
