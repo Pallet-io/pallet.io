503dac1c14ab77dc6681f40bade5c5c6ccba2891
DeemoHarlos <cces4415@gmail.com>
2018-02-04 20:38:19 +0800

    add button, minor adjustment on title layout

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ generateHexBlocks(howBlocks)
 
 generateRoadmap()
 
-$("#title #whitepaper").click(()=>{alert('To be announced')})
+function toBeAnnounced(a){
+	if(a == "en") alert("To be announced!")
+	if(a == "zh-tw") alert("即將發佈！")
+	if(a == "zh-cn") alert("即将发布！")
+}
 
-document.addEventListener('scroll', function(){
+document.addEventListener("scroll", function(){
 	if ($("html").scrollTop < 400) $("#navbar").style.opacity = 0;
 	else $("#navbar").style.opacity = 1;
 });
